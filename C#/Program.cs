cd01c02b7d2b6aef84beeff3a09a8b88bb3915e1
MEUUUL <MelAbgrall@users.noreply.github.com>
2017-10-26 11:52:27 +0200

    adding c#

---
+++ C#/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Print(string somethingFromUser)
+        {
+            Console.WriteLine("you wrote" + somethingFromUser +"\n");
+        }
+
+        #region region main
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! \n new line \t tab");
+            byte mybyte; //0-255
+            short myshort;
+            int myint;
+            long mylongint;
+            float myfloat;
+            double mydoublefloat;
+
+            Console.WriteLine("say something");
+            var something = Console.ReadLine();
+
+            Console.WriteLine("do you want to print it? [yes/no]");
+            string line = Console.ReadLine();
+            switch (line)
+            {
+                case "y":
+                case "yes":
+                    Print(something);
+                    break;
+
+                case "nice square":
+                    NiceSquare square = new NiceSquare(something);
+                    Console.WriteLine(square.getmySquare());
+                    break;
+
+                default:
+                    break;
+            }
+            Console.WriteLine("\n end of programm, press any key to exit...");
+            Console.ReadKey();
+        }
+        
+        #endregion
+    }
+}
---
+++ C#/nicesquare.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    
+    class NiceSquare
+    {
+        private string mySquare;
+        
+        /**
+         * NiceSquare render "something" inside a nice square
+        **/
+        public NiceSquare(string something)
+        {
+            
+
+            mySquare = "+";
+
+            for (byte i = 0; i<something.Length + 2 ; i++)
+            {
+                mySquare = mySquare + "-";
+            }
+
+            mySquare = mySquare + "+\n| " + something + " |\n+";
+
+            for (byte i = 0; i <= something.Length +1 ; i++)
+            {
+                mySquare = mySquare + "-";
+            }
+
+            mySquare = mySquare + "+\n";
+
+        }
+
+        public string getmySquare()
+        {
+            return mySquare;
+        }
+    }
+}
